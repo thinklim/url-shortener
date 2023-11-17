34c10c1a577a174403935025ef93678a6020df41
thinklim <sagalim3983@gmail.com>
2023-11-17 14:28:38 +0900

    커스텀 사용자 Admin 페이지 추가

---
+++ accounts/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+from .models import CustomUser
+
+
+class CustomUserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Password confirmtion", widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = [
+            "email",
+        ]
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+
+        if password1 and password2 and password1 != password2:
+            raise ValueError("비밀번호가 다릅니다")
+
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+
+        if commit:
+            user.save()
+
+        return user
+
+
+class CustomUserChangeForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ["password", "is_active", "is_admin"]
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+
+        if commit:
+            user.save()
+
+        return user
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,6 @@
 import pytest
 
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
 
@@ -11,3 +12,23 @@ def test_user_create():
     CustomUser.objects.create_user(email, password)
 
     assert CustomUser.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_user_creation_form():
+    form = CustomUserCreationForm(
+        data={
+            "email": "test@example.com",
+            "password1": "test_example",
+            "password2": "test_example",
+        }
+    )
+
+    assert form.is_valid()
+
+
+@pytest.mark.django_db
+def test_usr_change_form():
+    form = CustomUserChangeForm(data={"password": "test_exam"})
+
+    assert form.is_valid()
